060586cd77113556ebbe851e76696dce98e7a3c8
Telwak <tomasz.telwak@telwak.pl>
2017-07-31 08:46:57 +0200

    Relase

---
+++ Stoper/Project1/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.UI.WebControls;
+
+namespace Project1.App_Start
+{
+    public class BundleConfig
+    {
+
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+      
+
+        
+            BundleTable.EnableOptimizations = true;
+
+            StyleBundle StyleSheet = new StyleBundle("~/Style/Style");
+             StyleSheet.Include("~/Style/StyleSheet1.css");
+
+        }
+
+
+    }
+}
